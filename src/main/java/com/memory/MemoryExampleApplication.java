066e57be5d31de235db3fcb72e178feb5fbe5b2b
Gustavo Lobo <gustavo.lobo95@gmail.com>
2025-11-17 08:06:51 -0300

    Criando projeto

---
+++ src/main/java/com/memory/MemoryExampleApplication.java
@@ -0,0 +1,13 @@
+package com.memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryExampleApplication.class, args);
+	}
+
+}
